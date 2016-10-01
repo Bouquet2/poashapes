772566161dd98af50a4f66b09863a318f0b95d0c
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 12:47:33 +0200

    Add the test method for translate

--- test/Point2DTest.java
+++ test/Point2DTest.java
@@ -8,6 +8,20 @@ import static org.testng.Assert.*;
 public class Point2DTest {
     @Test
     public void testTranslate() throws Exception {
+        Point2D p = new Point2D(0,0);
+        p.translate(5,-3);
+        assertEquals(p.getX(), 5.0);
+        assertEquals(p.getY(), -3.0);
+
+        Point2D p2 = new Point2D(7,0);
+        p2.translate(0,5);
+        assertEquals(p2.getX(), 7.0);
+        assertEquals(p2.getY(), 5.0);
+
+        Point2D p3 = new Point2D(5,-3);
+        p3.translate(0,0);
+        assertEquals(p3.getX(), 5.0);
+        assertEquals(p3.getY(), -3.0);
     }
 
     @Test
