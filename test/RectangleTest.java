1e3009aa97a0d152f1594a623b25cec2752107ba
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:11:45 +0200

    Add the test class for Rectangle

---
+++ test/RectangleTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Valentin on 01/10/2016.
+ */
+public class RectangleTest {
+    @Test
+    public void testPerimeter() throws Exception {
+    }
+
+    @Test
+    public void testSurface() throws Exception {
+
+    }
+
+}
