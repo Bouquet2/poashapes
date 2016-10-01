56ff84ed91f5f9747d77ec9ee43a7b85335107b5
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:48:12 +0200

    Add test for Volume

--- test/SphereTest.java
+++ test/SphereTest.java
@@ -8,7 +8,18 @@ import static org.testng.Assert.*;
 public class SphereTest {
     @Test
     public void testVolume() throws Exception {
+        Sphere s = new Sphere(3);
+        assertEquals(s.volume(), 113.09733552923255);
 
+        Sphere s2 = new Sphere(0);
+        assertEquals(s2.volume(), 0.0);
+
+        Point3D p = new Point3D(5,9,15);
+        Sphere s3 = new Sphere(p,4);
+        assertEquals(s3.volume(), 268.082573106329);
+
+        Sphere s4 = new Sphere(8);
+        assertNotEquals(s2.volume(), 57.0);
     }
 
     @Test
