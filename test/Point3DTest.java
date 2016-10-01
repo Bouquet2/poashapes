f6e536bb174a3edf34cf8b761666a80fd6400ab9
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 16:28:12 +0200

    Add test for the method translate

--- test/Point3DTest.java
+++ test/Point3DTest.java
@@ -8,6 +8,23 @@ import static org.testng.Assert.*;
 public class Point3DTest {
     @Test
     public void testTranslate() throws Exception {
+        Point3D p = new Point3D(0,0,0);
+        p.translate(5,-3,4);
+        assertEquals(p.getX(), 5.0);
+        assertEquals(p.getY(), -3.0);
+        assertEquals(p.getZ(), 4);
+
+        Point3D p2 = new Point3D(7,0,0);
+        p2.translate(0,5,-5);
+        assertEquals(p2.getX(), 7.0);
+        assertEquals(p2.getY(), 5.0);
+        assertEquals(p2.getZ(), -5.0);
+
+        Point3D p3 = new Point3D(5,-3,10);
+        p3.translate(0,0);
+        assertEquals(p3.getX(), 5.0);
+        assertEquals(p3.getY(), -3.0);
+        assertEquals(p3.getZ(), 10.0);
     }
 
     @Test
