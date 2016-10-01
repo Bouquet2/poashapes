98ca5e1a2ebfbe7eaea59a50820aeed2f113bce7
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:57:40 +0200

    Add test for Volume

--- test/CuboidTest.java
+++ test/CuboidTest.java
@@ -24,7 +24,18 @@ public class CuboidTest {
 
     @Test
     public void testVolume() throws Exception {
+        Cuboid c = new Cuboid(2,4,6);
+        assertEquals(c.volume(), 88.0);
 
+        Cuboid c2 = new Cuboid(0,0,0);
+        assertEquals(c2.volume(), 0.0);
+
+        Point3D p = new Point3D(5,9,15);
+        Cuboid c3 = new Cuboid(p,4,6,8);
+        assertEquals(c3.volume(), 208.0);
+
+        Cuboid c4 = new Cuboid(4,7,9);
+        assertNotEquals(c4.volume(), 94.025);
     }
 
 }
