f0f4eefa05deccad6e263ac7c1b0c1e90884e692
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:28:32 +0200

    Add test for diameter

--- test/ConeTest.java
+++ test/ConeTest.java
@@ -8,6 +8,14 @@ import static org.testng.Assert.*;
 public class ConeTest {
     @Test
     public void testDiameter() throws Exception {
+        Cone c = new Cone(5,3);
+        assertEquals(c.diameter(), 6.0);
+
+        Cone c2 = new Cone(0,0);
+        assertEquals(c2.diameter(), 0.0);
+
+        Cone c3 = new Cone(15,8);
+        assertNotEquals(c3.diameter(), 15.0);
     }
 
     @Test
