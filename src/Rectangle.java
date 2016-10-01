ed441a2c3d6e780ffff9c5b2dd85fc9ed9265cdc
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:02:07 +0200

    Add class Rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,56 @@
+/**
+ * Created by Valentin on 01/10/2016.
+ */
+public class Rectangle extends Shape2D{
+
+    private double length;
+    private double width;
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+
+    public double surface() {
+        return length * width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Rectangle)) return false;
+
+        Rectangle rectangle = (Rectangle) o;
+
+        if (Double.compare(rectangle.getLength(), getLength()) != 0) return false;
+        return Double.compare(rectangle.getWidth(), getWidth()) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(getLength());
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getWidth());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                '}';
+    }
+}
