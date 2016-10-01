12abfa95220408288ac084ed1d9e4e664ab87bbb
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:12:16 +0200

    Add test for the method perimeter

--- test/RectangleTest.java
+++ test/RectangleTest.java
@@ -8,6 +8,15 @@ import static org.testng.Assert.*;
 public class RectangleTest {
     @Test
     public void testPerimeter() throws Exception {
+        Rectangle r = new Rectangle(10,5);
+        assertEquals(r.perimeter(), 30.0);
+
+        Rectangle r2 = new Rectangle(0,0);
+        assertEquals(r2.perimeter(), 0.0);
+
+        Point2D p = new Point2D(5,10);
+        Rectangle r3 = new Rectangle(3,7);
+        assertEquals(r3.perimeter(), 20.0);
     }
 
     @Test
