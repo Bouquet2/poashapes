b05501d1192ef5337408815e133bf4c385803c50
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:27:42 +0200

    Add test for class Cone

---
+++ test/ConeTest.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Valentin on 01/10/2016.
+ */
+public class ConeTest {
+    @Test
+    public void testDiameter() throws Exception {
+    }
+
+    @Test
+    public void testVolume() throws Exception {
+
+    }
+
+    @Test
+    public void testLateralSurface() throws Exception {
+
+    }
+
+    @Test
+    public void testSurface() throws Exception {
+
+    }
+
+}
