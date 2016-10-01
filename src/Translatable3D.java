636175375700fc098992357f9928bd3be975062f
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 16:52:29 +0200

    Fixed interface

--- src/Translatable3D.java
+++ src/Translatable3D.java
@@ -1,7 +1,7 @@
 /**
  * Created by Valentin on 01/10/2016.
  */
-public abstract class Translatable3D extends Translatable2D{
+public interface Translatable3D extends Translatable2D{
     @Override
     abstract Point2D getRefPoint();
 }
