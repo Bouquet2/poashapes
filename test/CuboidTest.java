a23d6b09a2b61f160959f122a8b9fd5e9a5e8663
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:57:00 +0200

    Add test for Surface

--- test/CuboidTest.java
+++ test/CuboidTest.java
@@ -8,7 +8,18 @@ import static org.testng.Assert.*;
 public class CuboidTest {
     @Test
     public void testSurface() throws Exception {
+        Cuboid c = new Cuboid(2,4,6);
+        assertEquals(c.surface(), 88.0);
 
+        Cuboid c2 = new Cuboid(0,0,0);
+        assertEquals(c2.surface(), 0.0);
+
+        Point3D p = new Point3D(5,9,15);
+        Cuboid c3 = new Cuboid(p,4,6,8);
+        assertEquals(c3.surface(), 208.0);
+
+        Cuboid c4 = new Cuboid(4,7,9);
+        assertNotEquals(c4.surface(), 94.025);
     }
 
     @Test
