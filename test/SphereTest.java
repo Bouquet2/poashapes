a6ecf50a747127d568acdf868f74302c7fa66a1d
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:39:16 +0200

    Add test for class Sphere

---
+++ test/SphereTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Valentin on 01/10/2016.
+ */
+public class SphereTest {
+    @Test
+    public void testVolume() throws Exception {
+
+    }
+
+    @Test
+    public void testSurface() throws Exception {
+
+    }
+
+}
