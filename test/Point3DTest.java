dcae4d10f176e7fa310bf344ae5cb5e712f7dc50
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 16:25:55 +0200

    Add the test class for Point3D

---
+++ test/Point3DTest.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Valentin on 01/10/2016.
+ */
+public class Point3DTest {
+    @Test
+    public void testTranslate() throws Exception {
+    }
+
+    @Test
+    public void testIsOrigin() throws Exception {
+
+    }
+
+    @Test
+    public void testDistance() throws Exception {
+
+    }
+
+}
