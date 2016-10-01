b48f1205102e51dfc1428c3e68c4b93675960833
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:50:41 +0200

    Add test class for Cuboid

---
+++ test/CuboidTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Valentin on 01/10/2016.
+ */
+public class CuboidTest {
+    @Test
+    public void testSurface() throws Exception {
+
+    }
+
+    @Test
+    public void testVolume() throws Exception {
+
+    }
+
+}
