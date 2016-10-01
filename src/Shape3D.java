dc3d3e4dd87e8e294a7e0c04eda36eb7f103a122
Valentin <valentin-bouquet@hotmail.fr>
2016-10-01 17:19:05 +0200

    Add missing constructors

--- src/Shape3D.java
+++ src/Shape3D.java
@@ -4,6 +4,14 @@
 public abstract class Shape3D extends Shape implements Translatable3D{
     private Point3D refPoint;
 
+    public Shape3D() {
+        this.refPoint = new Point3D();
+    }
+
+    public Shape3D(Point3D refPoint) {
+        this.refPoint = refPoint;
+    }
+
     public abstract double volume();
 
     public Point3D getRefPoint() {
