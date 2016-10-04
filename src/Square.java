dfc103f6eb58ae6c9d5c6c4d8b351c538931a1ad
Valentin <valentin-bouquet@hotmail.fr>
2016-10-04 17:24:33 +0200

    Fixed constructors

--- src/Square.java
+++ src/Square.java
@@ -11,6 +11,20 @@ public class Square extends Rectangle {
         super(refPoint, length, length);
     }
 
+    public Square(Point2D refPoint, double length, double width) throws RectangleException{
+        super(refPoint, length, width);
+        if(length != width) {
+            throw new RectangleException("Nouveau Rectangle : La largeur doit être égal à la haute");
+        }
+    }
+
+    public Square(double length, double width) throws RectangleException{
+        super(length, width);
+        if(length != width) {
+            throw new RectangleException("Nouveau Rectangle : La largeur doit être égal à la haute");
+        }
+    }
+
     @Override
     public String toString() {
         return "Rectangle{" +
